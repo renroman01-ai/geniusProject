e37cf7506c70b39c9d5d9ec01d31bd12df7bbd0c
ren.roman01-ai <ren.roman01@gmail.com>
2026-01-08 18:06:12 +0200

    format and save hw1

--- homework1/script.js
+++ homework1/script.js
@@ -5,7 +5,3 @@ name = "Іван";
 city = name;
 
 console.log(city);
-// Завдання №2
-let userName = prompt("Як вас звати?"); 
-alert("Hello, " + userName + "!"); // Це вікно не зникне, поки ви не натиснете ОК
-console.log("Hello, " + userName + "!");
