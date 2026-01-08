102e80aa5b6471d0c8795c8c297fa377960423d9
ren.roman01-ai <ren.roman01@gmail.com>
2026-01-08 18:05:24 +0200

    added task 2

--- homework1/script.js
+++ homework1/script.js
@@ -7,4 +7,5 @@ city = name;
 console.log(city);
 // Завдання №2
 let userName = prompt("Як вас звати?"); 
+alert("Hello, " + userName + "!"); // Це вікно не зникне, поки ви не натиснете ОК
 console.log("Hello, " + userName + "!");
