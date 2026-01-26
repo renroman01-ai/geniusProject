7e63beb0b0fae6e785b93cccfe87ea22e699be06
ren.roman01-ai <ren.roman01@gmail.com>
2026-01-26 17:48:04 +0200

    my real homework

--- homework1/script.js
+++ homework1/script.js
@@ -1,7 +1,24 @@
-let name;
+/*let name;
 let city;
-
 name = "Іван";
 city = name;
+console.log(city);*/
 
-console.log(city);
+//console.log(1 + 8);
+
+//складання з крапкою
+// console.log(0.1 + 0.2 === 0.3); // false
+// console.log(0.1 + 0.2); // 0.30000000000000004
+// console.log((0.1 * 10 + 0.2 * 10) / 10);
+
+
+// let user = {
+//   // об’єкт
+//   name: "Іван", // за ключем "name" зберігаємо значення "Іван"
+//   age: 30, // за ключем "age" зберігати значення "30"
+//   work: true,
+// };
+// console.log(user);
+
+
+
